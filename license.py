c382779b9d7ec5fff8e84728abcb6e7e7281f413
Alex Galonsky <alex@galonsky.com>
2012-04-11 17:01:24 -0400

    Regex changes, changing spaces to whitespace/asterisks, getting the type of the first match

--- license.py
+++ license.py
@@ -7,23 +7,16 @@ regexes = {
     'GPLv1': ["""GNU GENERAL PUBLIC LICENSE.*Version 1"""],
     'GPLv2': ["""GNU GENERAL PUBLIC LICENSE.*Version 2"""],
     'GPLv3': ["""GNU GENERAL PUBLIC LICENSE.*Version 3"""],
-    'AcademicFreeLicense': ["""The Academic Free License""", """This Academic Free License (the "License") applies to any original work of
-authorship (the "Original Work") whose owner (the "Licensor") has placed the
-following notice immediately following the copyright notice for the Original
-Work:"""],
+    'AcademicFreeLicense': ["""The Academic Free License""", """This Academic Free License (the "License") applies to any original work of authorship (the "Original Work") whose owner (the "Licensor") has placed the following notice immediately following the copyright notice for the Original Work:"""],
     'Apachev2': ["""Apache License.*Version 2\.0"""],
-    'BSD': ["""BSD License""","""Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:.*"""],
-    'MIT': ["""MIT License""","""Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files \(the "Software"\), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:"""],
+    'BSD': ["""BSD License""","""Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:"""],
+    'MIT': ["""MIT License""","""Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files \(the ['"]Software['"]\), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:"""],
     'FDL': ["""GNU Free Documentation License"""],
     'Ruby': ["""Ruby License""", """You may make and give away verbatim copies of the source form of this
     software without restriction, provided that you retain ALL of the
     original copyright notices and associated disclaimers."""],
-    'ArtisticLicense': ["""The Artistic License""", """This license establishes the terms under which a given free software
-Package may be copied, modified, distributed, and/or redistributed\.
-The intent is that the Copyright Holder maintains some artistic
-control over the development of that Package while still keeping the
-Package available as open source and free software\."""]
+    'ArtisticLicense': ["""The Artistic License""", """This license establishes the terms under which a given free software Package may be copied, modified, distributed, and/or redistributed\. The intent is that the Copyright Holder maintains some artistic control over the development of that Package while still keeping the Package available as open source and free software\."""],
+    'ISC': ["""Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies\."""]
 }
 
 patterns = {}
@@ -31,14 +24,20 @@ patterns = {}
 for key in regexes:
     patterns[key] = []
     for regex in regexes[key]:
-        patterns[key].append(re.compile(regex, re.IGNORECASE))
+        newregex = re.sub(' ', '[\s\*]+', regex)
+        patterns[key].append(re.compile(newregex, re.IGNORECASE | re.DOTALL))
 
 
 def getLicenseType(license):
+    bestmatch = None
     for licenseType in patterns:
         for pattern in patterns[licenseType]:
-            if pattern.search(license):
-                return licenseType
+            match = pattern.search(license)
+            if match:
+                if bestmatch == None or match.start(0) < bestmatch['start']:
+                    bestmatch = {'type': licenseType, 'start': match.start(0)}
+    if not bestmatch == None:
+        return bestmatch['type']
     return None
 
 
