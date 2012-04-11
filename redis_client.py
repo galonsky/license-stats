b88b8950365b8f0c58833188621aa65ab3f9aa90
Alex Galonsky <alex@galonsky.com>
2012-04-11 01:14:11 -0400

    Redis client methods

--- redis_client.py
+++ redis_client.py
@@ -5,3 +5,27 @@ config = ConfigParser.ConfigParser()
 config.read('config.cfg')
 
 r = redis.StrictRedis(host=config.get('redis', 'host'), port=config.getint('redis', 'port'), db=config.getint('redis', 'db'))
+
+
+def addUser(user):
+    return r.sadd('users', user)
+
+
+def getUser():
+    return r.spop('users')
+
+
+def addRepo(repo):
+    return r.sadd('repos', repo)
+
+
+def getRepo():
+    return r.spop('repos')
+
+
+def numUsers():
+    return r.scard('users')
+
+
+def numRepos():
+    return r.scard('repos')
