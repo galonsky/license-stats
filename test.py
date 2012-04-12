737b55f6fa47048dd948f1efcf00c0fafab760fe
Alex Galonsky <alex@galonsky.com>
2012-04-11 23:00:59 -0400

    Latest test script

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import repo
+import crawler
+import license
 
-repo.processRepo('https://github.com/gitpython-developers/GitPython')
+#repo.processRepo('https://github.com/gitpython-developers/GitPython')
+
+#crawler.crawlUsers(5000)
+crawler.crawlRepos()
+#license.updateLicenseTypes()
